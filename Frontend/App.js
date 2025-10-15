a8c73d7555cfb3f6695e34dc65b4fe5e2ecb149d
Sushant Sharma <itsofficialsushant@gmail.com>
2025-10-15 22:48:24 +0100

    feat : Main app entry

---
+++ Frontend/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './Login'; // make sure this path is correct based on where Login.jsx is
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
