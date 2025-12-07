8ad1f48bf366954e57eb5a2f36cb64550be6e3fa
Sushant Sharma <itsofficialsushant@gmail.com>
2025-12-07 13:33:00 +0100

    Devnagari font added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// Inject Google Fonts
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Noto+Sans+Devanagari:wght@400;500;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
+// const preeti = document.createElement('style');
+// preeti.innerHTML = `
+//   @font-face {
+//     font-family: 'Preeti';
+//     src: url('/fonts/Preeti.ttf') format('truetype');
+//     font-display: swap;
+//   }
+// `;
+// document.head.appendChild(preeti);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
