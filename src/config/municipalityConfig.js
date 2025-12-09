2842b158cd4ae7b657d3de8cd3bbb49ef04a9826
Sushant Sharma <itsofficialsushant@gmail.com>
2025-12-09 15:39:57 +0530

    Tokens added in DomesticAnimalMaternityNutritionAllowance form

--- src/config/municipalityConfig.js
+++ src/config/municipalityConfig.js
@@ -3,6 +3,7 @@ export const MUNICIPALITY = {
   name: "नागार्जुन नगरपालिका",
   officeLine: "नगर कार्यपालिकाको कार्यालय, काठमाडौं",
   provinceLine: "बागमती प्रदेश, नेपाल",
+  district: "काठमाडौँ",
 
 // English
   englishMunicipality: "Nagarjun Municipality",
