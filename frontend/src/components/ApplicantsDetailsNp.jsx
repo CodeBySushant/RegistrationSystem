5b973b0f0a0673d0fef52caeca1cd45f3de54d89
Sushant Sharma <itsofficialsushant@gmail.com>
2026-04-01 01:02:58 +0530

    Component Created for Applicant Details

---
+++ frontend/src/components/ApplicantsDetailsNp.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+const ApplicantsDetailsNp = () => {
+  return (
+    <>
+      {/* --- Applicant Details Box --- */}
+      <div className="applicant-details-box">
+        <h3>निवेदकको विवरण</h3>
+        <div className="details-grid">
+          <div className="detail-group">
+            <label>
+              निवेदकको नाम<span className="required">*</span>
+            </label>
+            <input
+              name="applicant_name"
+              type="text"
+              className="detail-input bg-gray"
+              value={form.applicant_name}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="detail-group">
+            <label>
+              निवेदकको ठेगाना<span className="required">*</span>
+            </label>
+            <input
+              name="applicant_address"
+              type="text"
+              className="detail-input bg-gray"
+              value={form.applicant_address}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="detail-group">
+            <label>
+              निवेदकको नागरिकता नं.<span className="required">*</span>
+            </label>
+            <input
+              name="applicant_citizenship_no"
+              type="text"
+              className="detail-input bg-gray"
+              value={form.applicant_citizenship_no}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="detail-group">
+            <label>
+              निवेदकको फोन नं.<span className="required">*</span>
+            </label>
+            <input
+              name="applicant_phone"
+              type="text"
+              className="detail-input bg-gray"
+              value={form.applicant_phone}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ApplicantsDetailsNp;
